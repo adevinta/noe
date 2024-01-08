ef62978c56e7c09eb3b81677bd15302736b4d1d8
Miguel Bernabeu <miguel.bernabeu@lobber.eu>
2024-01-08 17:19:32 +0100

    Add `cluster-schedulable-archs` flag
     # Context
    
    In some cases we have seen that the common architectures of the images
    do not match the default preferred architecture. In those cases, one
    of the common architectures is selected, which may not match the
    available architectures in the cluster. In those cases, the Pod is
    always pending scheduling waiting for a node that will never exist.
    
     # Approach
    
    Allow passing a comma-separated list of architectures schedulable in
    the cluster. As we detect image's architecture support, we ignore
    those architectures that have not been specified as supported by the
    cluster.
    
    This new flag is optional and defaults to empty string (which disables
    the feature and keeps the prior behaviour).
    
     ## Startup fail if preferred architecture is not part of the supported architectures
    
    Startup will fail if the preferred architecture selected via
    commandline is not part of the list of schedulable architectures to
    prevent misconfigurations.
    
     ## Ignore preferred arch at Pod level if unsupported
    
    If a preferred architecture is indicated at Pod level, but it does not
    belong to the list of schedulable architectures, we ignore the preference
    and treat it as not specified. A log line is generated in that case.
    
     # Non-goal
    
     ## Detect architectures present in the cluster
    
    We do not want to prevent scheduling of Pods with architectures
    supported but which have no nodes currently in the cluster
    (downscaled) to avoid interfering with scale from 0 set ups.
    
    Change-Id: I0b85ab61fb3a267b41d938bd90f2e2d2050e5447
    Co-authored-by: Fabián Sellés Rosa <1088313+Fsero@users.noreply.github.com>

--- pkg/arch/hook.go
+++ pkg/arch/hook.go
@@ -110,13 +110,14 @@ func (w warning) Error() string {
 
 type HandlerOption func(*Handler)
 type Handler struct {
-	Client                client.Client
-	Registry              Registry
-	matchNodeLabels       []string
-	metrics               HandlerMetrics
-	decoder               *admission.Decoder
-	preferredArchitecture string
-	systemOS              string
+	Client                   client.Client
+	Registry                 Registry
+	matchNodeLabels          []string
+	metrics                  HandlerMetrics
+	decoder                  *admission.Decoder
+	preferredArchitecture    string
+	schedulableArchitectures []string
+	systemOS                 string
 }
 
 func NewHandler(client client.Client, registry Registry, opts ...HandlerOption) *Handler {
@@ -138,6 +139,12 @@ func WithMetricsRegistry(reg metrics.RegistererGatherer) HandlerOption {
 func WithArchitecture(arch string) HandlerOption {
 	return func(h *Handler) {
 		h.preferredArchitecture = arch
+	}
+}
+
+func WithSchedulableArchitectures(archs []string) HandlerOption {
+	return func(h *Handler) {
+		h.schedulableArchitectures = archs
 	}
 }
 
@@ -295,12 +302,16 @@ func (h *Handler) updatePodSpec(ctx context.Context, namespace string, podLabels
 
 	var preferredArchIsDefault bool
 	preferredArch, preferredArchDefined := podLabels["arch.noe.adevinta.com/preferred"]
+	if preferredArch != "" && !h.isArchSupported(preferredArch) {
+		log.DefaultLogger.WithContext(ctx).WithField("preferredArch", preferredArch).Println("ignoring unsupported user preferred architecture")
+		preferredArch = ""
+	}
 	if preferredArch == "" && h.preferredArchitecture != "" {
 		preferredArch = h.preferredArchitecture
 		preferredArchDefined = true
 		preferredArchIsDefault = true
 		ctx = log.AddLogFieldsToContext(ctx, logrus.Fields{"preferredArch": preferredArch})
-		log.DefaultLogger.WithContext(ctx).Println("selecting preferred architecture")
+		log.DefaultLogger.WithContext(ctx).Println("selecting default preferred architecture")
 	}
 
 	commonArchitectures := map[string]struct{}{}
@@ -323,6 +334,10 @@ func (h *Handler) updatePodSpec(ctx context.Context, namespace string, podLabels
 		for _, platform := range platforms {
 			if platform.OS != "" && platform.OS != h.systemOS {
 				log.DefaultLogger.WithContext(ctx).WithField("os", platform.OS).Info("Skipped OS does not match system's")
+				continue
+			}
+			if !h.isArchSupported(platform.Architecture) {
+				log.DefaultLogger.WithContext(ctx).WithField("arch", platform.OS).Info("Skipped arch does not match system's")
 				continue
 			}
 			imageArchitectures[platform.Architecture] = struct{}{}
@@ -496,6 +511,19 @@ func (h *Handler) Handle(ctx context.Context, req admission.Request) admission.R
 		resp = admission.Allowed(fmt.Sprintf("nothing to do for type %v", req.Kind.Kind))
 	}
 	return resp
+}
+
+func (h *Handler) isArchSupported(arch string) bool {
+	if len(h.schedulableArchitectures) == 0 {
+		return true
+	}
+
+	for _, schedulableArch := range h.schedulableArchitectures {
+		if arch == schedulableArch {
+			return true
+		}
+	}
+	return false
 }
 
 // Handler implements admission.DecoderInjector.
